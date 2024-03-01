0f1b0b2a0eb57cec87390c12b422d9851af6067c
Marcelo <zedlavolecram@gmail.com>
2024-02-29 22:19:17 -0300

    Refactor code for improved readability and maintainability

--- imread/imread_examples.py
+++ imread/imread_examples.py
@@ -1,22 +1,34 @@
-#!/usr/bin/python
 import cv2
 
+import os
+# change directory to the location of the image, imread/
+os.chdir(r"imread")
+
+
 # Read 8-bit grayscale image
-im = cv2.imread("earth-16-bit-per-channel.png", cv2.IMREAD_GRAYSCALE)
-print "flags :  cv2.IMREAD_GRAYSCALE"
-print "Size %s, type %s\n" % (im.shape,im.dtype)
+im = cv2.imread(r"earth-16-bit-per-channel.png", cv2.IMREAD_GRAYSCALE)
+if im is None:
+    print("Could not read the image.")
+    exit()
+
+print("flags :  cv2.IMREAD_GRAYSCALE")
+
+# print("Size %s, type %s\n" % (im.shape, im.dtype))º
+print(f'Size {im.shape}, type {im.dtype}\n')
 
 # Read 8-bit color image
 im = cv2.imread("earth-16-bit-per-channel.png", cv2.IMREAD_COLOR)
-print "flags : cv2.IMREAD_COLOR"
-print "Size %s, type %s\n" % (im.shape,im.dtype)
+print("flags : cv2.IMREAD_COLOR")
+print("Size %s, type %s\n" % (im.shape, im.dtype))
 
 # Read 16-bit color image
-im = cv2.imread("earth-16-bit-per-channel.png", cv2.IMREAD_ANYCOLOR | cv2.IMREAD_ANYDEPTH )
-print "flags :  cv2.IMREAD_ANYCOLOR | cv2.IMREAD_ANYDEPTH"
-print "Size %s, type %s\n" % (im.shape,im.dtype)
+im = cv2.imread(
+    "earth-16-bit-per-channel.png", cv2.IMREAD_ANYCOLOR | cv2.IMREAD_ANYDEPTH
+)
+print("flags :  cv2.IMREAD_ANYCOLOR | cv2.IMREAD_ANYDEPTH")
+print("Size %s, type %s\n" % (im.shape, im.dtype))
 
 # Read transparent PNG / TIFF image
 im = cv2.imread("earth-16-bit-per-channel.png", cv2.IMREAD_UNCHANGED)
-print "flags : cv2.IMREAD_UNCHANGED"
-print "Size %s, type %s\n" % (im.shape,im.dtype)
+print("flags : cv2.IMREAD_UNCHANGED")
+print("Size %s, type %s\n" % (im.shape, im.dtype))
---
+++ reading_writing_videos_in_opencv/video_read_from_file.py
@@ -0,0 +1,47 @@
+import cv2
+
+# Create a video capture object, in this case we are reading the video from a file
+vid_capture = cv2.VideoCapture("Resources/cam1.mp4")
+
+if not vid_capture.isOpened():
+    print("Error opening the video file")
+
+# Read fps and frame count
+else:
+    # Get frame rate information
+    # You can replace 5 with CAP_PROP_FPS as well, they are enumerations
+    fps = vid_capture.get(cv2.CAP_PROP_FPS)
+    print("Frames per second : ", fps, "FPS")
+
+    # Get frame count
+    # You can replace 7 with CAP_PROP_FRAME_COUNT as well, they are enumerations
+    frame_count = vid_capture.get(7)
+    print("Frame count : ", frame_count)
+
+
+#########################################
+# Read the video frame by frame
+#########################################
+while vid_capture.isOpened():
+
+    # vid_capture.read() methods returns a tuple, first element is a bool
+    # and the second is frame
+    ret, frame = vid_capture.read()
+
+    if ret:
+        # Display the frame scaled to 50% of the original size
+        frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
+
+        cv2.imshow("Frame", frame)
+
+        # 20 is in milliseconds, try to increase the value, say 50 and observe
+        key = cv2.waitKey(1)
+
+        if key == ord("q") or key == 27:
+            break
+    else:
+        break
+
+# Release the video capture object
+vid_capture.release()
+cv2.destroyAllWindows()
